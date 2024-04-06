4dd3cb44cdb8b93d4d658ae94bd48852e2f96ad3
SrikanthKamalla <srikanthkamalla2919@gmail.com>
2024-04-06 19:04:44 +0530

    2

---
+++ AverageOfNumbers.java
@@ -0,0 +1,11 @@
+public class AverageOfNumbers {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=50;
+        int sum=a+b+c;
+        int avg=sum/3;
+        System.out.println("Average of the numbers:" + avg);
+    }
+    
+}
